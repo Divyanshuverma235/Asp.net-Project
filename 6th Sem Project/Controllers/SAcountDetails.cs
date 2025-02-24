ab6a6dfc41dc77f66943ca8da685f188b40591a7
Divyanshu Raj <160339078+Divyanshuverma235@users.noreply.github.com>
2025-02-24 10:09:04 +0530

    UpdateAdd project files.

---
+++ 6th Sem Project/Controllers/SAcountDetails.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _6th_Sem_Project.Controllers
+{
+    public class SAcountDetails : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Settings()
+        {
+            return View();
+        }
+
+        public IActionResult EnrolledCourses()
+        {
+            return View();
+        }
+        public IActionResult Wishlisht()
+        {
+            return View();
+        }
+    }
+}
